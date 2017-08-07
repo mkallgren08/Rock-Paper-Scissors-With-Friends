319c81c7d9d7ad94b15b81fd38a860d516efb521
Michael <mkallgren08@gmail.com>
2017-08-07 17:18:43 -0400

    initial file uplaod

---
+++ javascript/rock_paper_scissors.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALrMRDk7vo4mOBQOv78yWt-zlILqPl650",
+    authDomain: "rock-paper-scissors-8cbc9.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-8cbc9.firebaseio.com",
+    projectId: "rock-paper-scissors-8cbc9",
+    storageBucket: "",
+    messagingSenderId: "522153913575"
+  };
+  firebase.initializeApp(config);
+
+  
