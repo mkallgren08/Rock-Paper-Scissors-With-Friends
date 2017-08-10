7bac1662da6216bee0b7947d7e0a2aa57b063977
Michael <mkallgren08@gmail.com>
2017-08-10 01:49:19 -0400

    Got the ID's relatively working!

--- javascript/rock_paper_scissors.js
+++ javascript/rock_paper_scissors.js
@@ -1,5 +1,8 @@
  // Initialize Firebase
 //=================================================================
+ $(document).ready(function() {
+ console.log("=================================================================")
+
   var config = {
     apiKey: "AIzaSyBvA9My1TC8DRmJaydWEUtaU9s8ypiuRKc",
     authDomain: "rock-paper-scissors-messaging.firebaseapp.com",
@@ -13,15 +16,19 @@
   // Get a reference to the root of the chat data.
 
   var msgDatabase = firebase.database();
-  console.log(msgDatabase)
-
-  var messages = [];
+  //console.log(msgDatabase)
 
   var numPlayers;
 
   var Player1;
 
   var Player2;
+
+  var players = []
+
+  var myID = "";
+
+  var myQueueNumber
 //======================================================
   // USER/PLAYER LOGS
 //======================================================
@@ -34,77 +41,117 @@ var connectionsRef = msgDatabase.ref("/connections");
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 var connectedRef = msgDatabase.ref(".info/connected");
 
+// Writes the number of active wathcers to the HTML
+connectionsRef.on("value", function(snap) {
+	console.log("=================================================================")
+  $("#watchers").html(snap.numChildren());
+  numPlayers = msgDatabase.ref("numPlayers").set(snap.numChildren());
+  console.log("Number of Watchers: " + snap.numChildren())
+
+});
+
+function makeMainPlayerIDs(snapshot){
+	var data = Object.keys(snapshot.val());
+	    console.log(data);
+	    players = [];
+	    for( var i = 0; i < 2; i++) {
+	        	// "?" = value-if-true, ":" value-if-false
+	            name = data[i]
+	            //console.log(name);
+	            if (i == 0) {
+	            	Player1 = name;
+	            	console.log("Player1 ID: " + Player1);
+	            	players.push(Player1)
+	            	console.log("Player array: " + players)
+	            } else if ( i === 1){
+	            	Player2 = name;
+	            	console.log("Player2 ID: " + Player2);
+	            	players.push(Player2)
+	            	console.log("Player array: " + players)
+	            }	            
+		}
+};
+
 // When the client's connection state changes...
 connectedRef.on("value", function(snap) {
   // If they are connected..
   if (snap.val()) {
     // Add user to the connections list.
     var con = connectionsRef.push(true);
-    // Remove user from the connection list when they disconnect.
+    
+    // Remove user from the connection list when they disconnect.;
     con.onDisconnect().remove();
+
+    // Creates the IDs for player 1 and player 2
+    connectionsRef.on("value", function(snapshot) {
+			makeMainPlayerIDs(snapshot);
+			highlightPlayer(myID);
+	});
+
   }
-});
-
-// When first loaded or when the connections list changes...
-
-connectionsRef.on("value", function(snap) {
-
-  // Display the viewer count in the html.
-  // The number of online users is the number of children in the connections list.
-  $("#watchers").html(snap.numChildren());
-  numPlayers = msgDatabase.ref("numPlayers").set(snap.numChildren());
-  console.log(snap.numChildren())
 
 });
 
-msgDatabase.ref("connections").on("value", function(snapshot) {
-	var data = Object.keys(snapshot.val());
-    console.log(data);
-    for( var i = 0; i < 2; i++) {
-        	// "?" = value-if-true, ":" value-if-false
-            name = data[i]
-            console.log(name);
-            if (i == 0) {
-            	player1 = name;
-            	console.log("Player1 ID: " + player1);
-            } else if ( i === 1){
-            	player2 = name;
-            	console.log("Player2 ID: " + player2);
-            }
-        }
-});
+function highlightPlayer(myID){
+ if (myID !== players[0] && myID !== players[1]){
+ 	$(".player2-panel").css({"border" : "0px solid transparent"},
+ 		{"background-color": "white"}
+ 		)
+ 	 $(".player1-panel").css({"border" : "0px solid transparent"},
+ 		{"background-color": "white"}
+ 		)		
+ 	} else if (myID === players[1]){
+ 	$(".player2-panel").css({"border" : "5px solid red"},
+ 		{"background-color": "red"}
+ 		)
+ 	$(".player1-panel").css({"border" : "0px solid transparent"},
+ 		{"background-color": "white"}
+ 		)
+ 	} else{
+ 	$(".player1-panel").css({"border" : "5px solid blue"},
+ 		{"background-color": "blue"}
+ 		)
+ 	 $(".player2-panel").css({"border" : "0px solid transparent"},
+ 		{"background-color": "white"}
+ 		)		
+ 	}
+}
+//This assigns you the ID at the end of the queue - don't reload! 
+connectionsRef.once("value", function(snap) {
+		var data2 = Object.keys(snap.val())
+		console.log("data2: " + data2)
+		myID = data2[data2.length-1]
+		console.log("myID.2: " + myID);
+		highlightPlayer(myID)
+	});
 
+msgDatabase.ref("/connections").on("child_changed", function(snap){
+	highlightPlayer(myID)
+})
 
-var numPlayers = msgDatabase.ref('numPlayers').on("value", function(snapshot) {
-
-				});
-
-
-
-
-
-
-  // // When the user presses enter on the message input, write the message to firebase.
-  // $("#messageInput").keypress(function (e) {
-  //   if (e.keyCode == 13) {
-  //     var name = $("#nameInput").val();
-  //     var text = $("#messageInput").val();
-  //     msgDatabase.push({name:name, text:text});
-  //     $("#messageInput").val("");
-  //   }
-  // });
-
-  // // Add a callback that is triggered for each chat message.
-  // msgDatabase.on("child_added", function (snapshot) {
-  //   var message = snapshot.val();
-  //   $("<div/>").text(message.text).prepend($("<em/>")
-  //     .text(message.name + ": ")).appendTo($("#messagesDiv"));
-  //   $("#messagesDiv")[0].scrollTop = $("#messagesDiv")[0].scrollHeight;
-  // });
+function disconnectAlert(){
+	var a = $("<p>")
+	a.text("A player has left!")
+	$("#messagesDiv").prepend(a);
+};
 
 //===========================================================================================
 // CHAT FUNCTIONS!
 //===========================================================================================
+function clearMsgLogs(roomToDelete){
+	msgDatabase.ref(roomToDelete).remove();
+	msgDatabase.ref(roomToDelete).push({
+         message: "Logs cleared!",
+	});
+	msgDatabase.ref(roomToDelete).once("value", function(snapshot, prevChildKey){
+	   var newPost = snapshot.val()
+  		var n = $("<p>")
+		n.text("Logs cleared!")
+   		$("#messagesDiv").html(n);
+	});
+};
+
+
 function saveToList(event) {
   	$("input").keydown(function(event){
 
@@ -113,7 +160,7 @@ function saveToList(event) {
 	        var messageText = $('#messageText').val().trim();
 	        if (messageText.length > 0) {
 	            saveMsgToFB(messageText);
-	            makeTextOutput(messageText);
+	            //makeTextOutput(messageText);
 	        }
 	        $('#messageText').val("");
 	        return false;
@@ -131,63 +178,69 @@ function makeTextOutput(messageText){
 function saveMsgToFB(messageText) {
     // this will save data to Firebase
     msgDatabase.ref("room1").push({
-        message: "Player 1: " + messageText,
+        message: "Player1: " + messageText,
     });
 };
- 
-// function refreshUI(messageLog) {
-//     var lis = '';
-//     for (var i = 0; i < messageLog.length; i++) {
-//         lis += '<p data-key="' + messageLog[i].key + '">' + messageLog[i].name + '</>';
-//     };
-//     $("#messagedDiv").html(lis);
-// };
  
 // this will get fired on inital load as well as 
 // whenever there is a change in the data
 console.log("You're here!")
-msgDatabase.ref('room1').once("value", function(snapshot) {
-    var data = snapshot.val();
-    console.log(data);
-    var messageLog = [];
-    for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-        	// "?" = value-if-true, ":" value-if-false
-            name = data[key].message ? data[key].message : '';
-            console.log(name);
-            if (name.trim().length > 0) {
-            	var m = $("<p>")
-            	m.text(name)
-                $("#messagesDiv").prepend(m);
-                }
-            }
-        }
-    });
 // Retrieve new posts as they are added to our database
 msgDatabase.ref('room1').on("child_added", function(snapshot, prevChildKey) {
   var newPost = snapshot.val();
   console.log("Last Message: " + newPost.message);
 
   var n = $("<p>")
-	n.text(newPost)
+	n.text(newPost.message)
    	$("#messagesDiv").prepend(n);
 });
-    // refresh the UI
-//     refreshUI(messageLog);
-// });
+
 saveToList();
+
+$("#clear-logs").on("click", function(event){
+	clearMsgLogs('room1');
+});
+
 
 //========================================================
 // GAMEPLAY FUNCTIONS
 //========================================================
 
-function savePickToFB(playerPick) {
-    // this will save data to Firebase
-    msgDatabase.ref("room1").push({
-        message: "Player 1: " + messageText,
+var arena = msgDatabase.ref("arena")
+var p1ticker = 0;
+var p2ticker = 0;
+
+
+$(".p1-weapon-pick").click( function(){
+	if (myID === players[0] && p1ticker === 0){
+	var p1weaponPick = $(this).val()
+	p1ticker++
+	console.log(p1weaponPick);
+	saveWeaponToFB(Player1, p1weaponPick);
+	}
+});
+
+$(".p2-weapon-pick").click( function(){
+	if (myID === players[1] && p2ticker === 0){
+	var p2weaponPick = $(this).val()
+	p2ticker++
+	console.log(p2weaponPick);
+	saveWeaponToFB(Player2, p2weaponPick);
+	}
+});
+
+
+function saveWeaponToFB(player, weaponPick) {
+    msgDatabase.ref("arena").push({
+        player: weaponPick,
     });
 };
- 
-        
+
+//console.log("Player 1 string ID: " + players[0].toString())
+msgDatabase.ref().on("value", function(snap){
+console.log("My string ID: " + myID)
+
+
+});      
     
-  
+ }); 
